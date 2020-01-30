f5687a54b3b52b63bac8c7027f0913fe528eafaf
tkcco <asd33tyu@gmail.com>
2020-01-30 22:08:17 +0900

    add eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
 }
